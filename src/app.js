0b4f5f740bb03254494413c640cbbf904537e6fa
tranhieu956230 <tranhieu956230@gmail.com>
2018-05-05 13:47:15 +0700

    Fix Route

--- src/app.js
+++ src/app.js
@@ -1,19 +1,14 @@
 
 import React from "react"
-import { Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter ,Route } from 'react-router-dom';
 import Home from "./Home/Home"
-import Login from "./Login/Login"
-import Register from "./Register/Register"
+
 class App extends React.Component {
     render() {
         return (
-            <Router>
-                <Switch>
-                    <Route exact path='/' component={Home} />
-                    <Route exact path='/login' component={Login} />
-                    <Route exact path='/register' component={Register} />
-                </Switch>
-            </Router>
+           <BrowserRouter>
+            <Route exact path='/' component={Home}/>
+           </BrowserRouter>
         )
     }
 }
