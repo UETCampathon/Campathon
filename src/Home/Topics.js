d04f2dfeac395c018eadb66ccb36a701c04deb24
tranhieu956230 <tranhieu956230@gmail.com>
2018-05-05 18:49:06 +0700

    Update Login Register

---
+++ src/Home/Topics.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+class Topics extends React.Component {
+    render() {
+        return (
+            <div className="col-sm-3">
+                <div className="card">
+                    <img className="card-img-top img-fluid img_card" src="images/1.png" alt="Card image cap" />
+                    <div className="card-block">
+                        <h4 className="card-title" />
+                        <a href className="btn btn-primary chuyen">Button</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Topics
--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,32 +1,61 @@
 import React from "react"
 import './Login.css'
-
+import {Redirect} from "react-router-dom"
+import {login} from "../Service/Services"
 class Login extends React.Component {
+    state = {
+        email: "" ,
+        password: "",
+        errorhandling: "",
+        redirect: false,
+    }
+    handleEmailChange = (e) => {
+        this.setState({email: e.target.value})
+    }
+    handlePasswordChange = (e) => {
+        this.setState({password: e.target.value})
+        
+    }
+    handleSubmit = () =>{
+        login({email:this.state.email,password:this.state.password}).then(res => {
+            if(res.success){
+                   this.setState({redirect: true})
+                   sessionStorage.setItem("accessToken",res.data.accessToken)
+            }
+            else this.setState({errorhandling: res.message})
+        })
+    }
+    redirect = () => {
+        return <Redirect to="/" />
+    }
     render() {
         return (
             <div className="loginPage">
-
+                {(this.state.redirect) ? this.redirect() : null}
                 <div className="contentLogin">
                     <h3>Log in to your Account</h3>
+                    {this.state.errorhandling}
                     <form>
                         <div className="form-group">
 
                             <div className="form-group">
                                 <label htmlFor="inputEmail">Email address</label>
                                 <input type="email" className="form-control" id="FormControlFullName"
-                                       placeholder="Full Name"/>
+                                       placeholder="Full Name"
+                                       onChange={this.handleEmailChange}/>
                             </div>
 
                             <div className="form-group">
                                 <div className="form-group">
                                     <label htmlFor="inputPassword">Password</label>
                                     <input type="password" className="form-control" id="inputPassword1"
-                                           placeholder="Password"/>
+                                           placeholder="Password"
+                                           onChange={this.handlePasswordChange}/>
                                 </div>
 
                             </div>
                             <div className="loginButton">
-                                <button type="button" className="btn btn-primary">Log In</button>
+                                <button type="button" className="btn btn-primary" onClick={this.handleSubmit}>Log In</button>
                             </div>
                         </div>
                     </form>
