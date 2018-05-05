8eb69c15f38c739d6ae7c85230ad7012a83ddfaf
vansi99 <vansido99@gmail.com>
2018-05-05 17:31:03 +0700

    change

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,9 +1,37 @@
 import React from "react"
+import './Login.css'
 
 class Login extends React.Component {
     render() {
         return (
-                <h1> Login</h1>
+            <div className="loginPage">
+
+                <div className="contentLogin">
+                    <h3>Log in to your Account</h3>
+                    <form>
+                        <div className="form-group">
+
+                            <div className="form-group">
+                                <label htmlFor="inputEmail">Email address</label>
+                                <input type="email" className="form-control" id="FormControlFullName"
+                                       placeholder="Full Name"/>
+                            </div>
+
+                            <div className="form-group">
+                                <div className="form-group">
+                                    <label htmlFor="inputPassword">Password</label>
+                                    <input type="password" className="form-control" id="inputPassword1"
+                                           placeholder="Password"/>
+                                </div>
+
+                            </div>
+                            <div className="loginButton">
+                                <button type="button" className="btn btn-primary">Log In</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         )
     }
 }
--- src/Register/Register.js
+++ src/Register/Register.js
@@ -6,27 +6,38 @@ class Register extends Component {
     render() {
         return (
             <div className="RegisterPage">
-                <div className='headerRegis'>
-                    <h1>REGISTER</h1>
-                </div>
+
                 <div className="contentRegister">
-                    <h3>Create your Account</h3>
+                    <h1>Create your Account</h1>
                     <form>
                         <div className="form-group">
 
                             <div className="form-group">
-                                <input type="email" className="form-control" id="FormControlFullName"
+                                <label htmlFor="InputFullname">FullName</label>
+                                <input type="text" className="form-control" id="FormControlFullName"
                                        placeholder="Full Name"/>
                             </div>
 
                             <div className="form-group">
+                                <label htmlFor="inputEmail">Your Email</label>
                                 <input type="email" className="form-control" id="inputEmail1"
                                        aria-describedby="emailHelp" placeholder="Enter email"/>
                             </div>
 
                             <div className="form-group">
-                                <input type="password" className="form-control" id="exampleInputPassword1"
+                                <label htmlFor="InputPassWord">Password</label>
+                                <input type="password" className="form-control" id="inputPassword"
                                        placeholder="Password"/>
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="InputConfirmPassWord">Confirm Password</label>
+                                <input type="password" className="form-control" id="inputConfirmPassword"
+                                       placeholder="Confirm Password"/>
+                            </div>
+                            
+                            <div className="regisButton">
+                                <button type="button" className="btn btn-primary">Sign Up</button>
                             </div>
                         </div>
 
