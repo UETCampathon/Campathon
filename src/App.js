e17de097b085dd5107b0261dbef1271db592fb2c
Quan <zed.quan99@gmail.com>
2018-05-05 20:00:50 +0700

    change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react"
 import Login from './Login/Login'
 import Register from "./Register/Register"
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {Route, Switch } from 'react-router-dom';
 import Home from "./Home/Home"
 import Navbar from "./NavBar";
 import Training from "./Training/Training"
