02c29023db9cf9cb97adbc81077383d914ed5baa
tranhieu956230 <tranhieu956230@gmail.com>
2018-05-06 02:59:55 +0700

    Hieu

--- src/Training/Image.js
+++ src/Training/Image.js
@@ -1,9 +1,12 @@
 import React from "react"
 
 class Image extends React.Component {
+    choose = () => {
+        this.
+    }
     render() {
         return (
-            <div className="col-sm-3 col-md-3 col-sx-10 push-sm-2 khoi1">
+            <div className="col-sm-3 col-md-3 col-sx-10 push-sm-2 khoi1" onClick={this.choose}>
                 <div className="trang" />
                 <div className="front">
                     <img src={this.props.src} />
