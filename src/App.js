b8cd4e9413327a96b93fec9e2332e93076ad70c7
Flashxu <dinhxuan9807@gmail.com>
2018-05-05 10:03:04 +0700

    up file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <nav className="navbar navbar-dark bg-inverse">
+              <div className="container">
+                  <a className="navbar-brand" href="index.html">Home</a>
+                  <a className="navbar-brand" href="training.html">Training</a>
+                  <a className="navbar-brand" href="create.html">Create</a>
+              </div>
+          </nav> {/* end menu */}
+
       </div>
     );
   }
