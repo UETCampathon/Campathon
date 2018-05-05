6bb622fb98c3f3b6201bac7827e5075e3b62076f
Flashxu <dinhxuan9807@gmail.com>
2018-05-06 03:31:32 +0700

    update lan 2

--- src/Home/Topics.js
+++ src/Home/Topics.js
@@ -12,7 +12,7 @@ class Topics extends React.Component {
     }
     render() {
         return (
-            <div className="col-sm-3">
+            <div className="col-sm-3 col-xs-6">
                 {(this.state.redirect) ? this.redirect() : null}
                 <div className="card">
                     <img className="card-img-top img-fluid img_card" src={this.props.src} alt="Card image cap" />
