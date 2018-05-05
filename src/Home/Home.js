25d186815326c73dfec0808322062cc5a6172479
tranhieu956230 <tranhieu956230@gmail.com>
2018-05-05 18:54:18 +0700

    Hieu Update

---
+++ src/Home/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Home.css';
+import Training from "./Training"
+import NavBar from "./NavBar"
+import DashBoard from "./DashBoard"
+class App extends Component {
+    render() {
+       
+        return (
+            <div className="App">
+                <div>
+                    <NavBar />
+                    <DashBoard />
+                    <Training />
+                </div>
+            </div>
+        );
+    }
+}
+export default App;
